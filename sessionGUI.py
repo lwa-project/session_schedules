949df7bdd9a41270fa66d08c700e703a3324da07
Jayce Dowell <jayce.dowell@gmail.com>
2011-04-04 22:48:17 +0000

    Cleaned up after the false commit in r367.  Added a 'check for unsaved changes' feature to quit.  Made a nicer looking data volume dialog box.

--- sessionGUI.py
+++ sessionGUI.py
@@ -522,31 +522,28 @@ class SDFCreator(wx.Frame):
 		"""Display a message window showing the data used for each observation 
 		and the total data volume."""
 		
-		VolumeInfo(self, -1, 'Estimated Data Volue')
-		
-		#out = 'Estimated Data Volume:\n'
-		
-		#i = 0
-		#tot = 0
-		#for obs in self.project.sessions[0].observations:
-			#out += "Observation #%2i: %6.2f GB\n" % (i+1, obs.dataVolume/1024.0**3)
-			#tot += obs.dataVolume
-			#i += 1
-		
-		#out += "--------------------------\n"
-		#out += "Total: %7.2f GB\n" % (tot/1024.0**3,)
-		
-		#wx.MessageBox(out, 'Data Volume')
+		VolumeInfo(self)
 	
 	def onAbout(self, event):
 		"""Display a ver very very bried 'about' window"""
 		
-		wx.MessageBox('GUI interface for session definition file creation.', 'About')
+		dialog = wx.MessageDialog(self, 'GUI interface for session definition file creation.', 'About', style=wx.OK|wx.ICON_INFORMATION)
+		if dialog.ShowModal() == wx.ID_OK:
+			pass
+		dialog.Destroy()
 	
 	def onQuit(self, event):
 		"""Quit the main window."""
 		
-		self.Close()
+		if self.edited:
+			dialog = wx.MessageDialog(self, 'The current session defintion file has changes that have not been saved.\n\nExit anyways?', 'Confirm Quit', style=wx.YES_NO|wx.NO_DEFAULT|wx.ICON_EXCLAMATION)
+			
+			if dialog.ShowModal() == wx.ID_YES:
+				self.Close()
+			else:
+				pass
+		else:
+			self.Close()
 		
 	def addColumns(self):
 		"""Add the various columns to the main window based on the type of 
@@ -1465,59 +1462,75 @@ class TimeseriesDisplay(wx.Frame):
 		self.Close()
 
 
-class VolumeInfo(wx.Dialog):
-	def __init__ (self, parent, ID, title):
-		nObs = len(parent.project.sessions[0].observations)
-		
-		wx.Dialog.__init__(self, parent, ID, 'Estimated Data Volume', size=(360, 370))
+ID_VOL_INFO_OK = 511
+
+class VolumeInfo(wx.Frame):
+	def __init__ (self, parent):
+		nObs = len(parent.project.sessions[0].observations)		
+		wx.Frame.__init__(self, parent, title='Estimated Data Volume', size=(300, nObs*20+120))
 		
 		self.parent = parent
 		
 		self.initUI()
 		self.initEvents()
-		self.ShowModal()
-		self.Destroy()
+		self.Show()
 		
 	def initUI(self):
+		row = 0
+		panel = wx.Panel(self)
+		sizer = wx.GridBagSizer(5, 5)
+		
 		font = wx.SystemSettings_GetFont(wx.SYS_SYSTEM_FONT)
 		font.SetPointSize(font.GetPointSize()+2)
 		
-		dataText = wx.StaticText(self, -1, 'Estimated Data Volume:')
+		dataText = wx.StaticText(panel, label='Estimated Data Volume:')
 		dataText.SetFont(font)
+		sizer.Add(dataText, pos=(row+0, 0), span=(1, 3), flag=wx.ALIGN_CENTER, border=5)
 	
-		wx.StaticLine(self, -1, (25, 50), (300,1))
+		line = wx.StaticLine(panel)
+		sizer.Add(line, pos=(row+1, 0), span=(1, 3), flag=wx.EXPAND|wx.BOTTOM, border=10)
+		
+		row += 2
 
-		wx.StaticText(self, -1, 'Slovakia', (25, 80))
-		wx.StaticText(self, -1, 'Hungary', (25, 100))
-		wx.StaticText(self, -1, 'Poland', (25, 120))
-		wx.StaticText(self, -1, 'Czech Republic', (25, 140))
-		wx.StaticText(self, -1, 'Germany', (25, 160))
-		wx.StaticText(self, -1, 'Slovenia', (25, 180))
-		wx.StaticText(self, -1, 'Austria', (25, 200))
-		wx.StaticText(self, -1, 'Switzerland', (25, 220))
+		observationCount = 1
+		totalData = 0
+		for obs in self.parent.project.sessions[0].observations:
+			idText = wx.StaticText(panel, label='Observation #%i' % observationCount)
+			tpText = wx.StaticText(panel, label=obs.mode)
+			dvText = wx.StaticText(panel, label='%.2f GB' % (obs.dataVolume/1024.0**3,))
+			
+			sizer.Add(idText, pos=(row+0, 0), flag=wx.ALIGN_LEFT, border=5)
+			sizer.Add(tpText, pos=(row+0, 1), flag=wx.ALIGN_CENTER, border=5)
+			sizer.Add(dvText, pos=(row+0, 2), flag=wx.ALIGN_RIGHT, border=5)
+			
+			observationCount += 1
+			totalData += obs.dataVolume
+			row += 1
+			
+		line = wx.StaticLine(panel)
+		sizer.Add(line, pos=(row+0, 0), span=(1, 3), flag=wx.EXPAND|wx.BOTTOM, border=10)
 
-		wx.StaticText(self, -1, '5 379 000', (250, 80))
-		wx.StaticText(self, -1, '10 084 000', (250, 100))
-		wx.StaticText(self, -1, '38 635 000', (250, 120))
-		wx.StaticText(self, -1, '10 240 000', (250, 140))
-		wx.StaticText(self, -1, '82 443 000', (250, 160))
-		wx.StaticText(self, -1, '2 001 000', (250, 180))
-		wx.StaticText(self, -1, '8 032 000', (250, 200))
-		wx.StaticText(self, -1, '7 288 000', (250, 220))
+		row += 1
 
-		wx.StaticLine(self, -1, (25, 260), (300,1))
+		ttText = wx.StaticText(panel, label='Total:')
+		ttText.SetFont(font)
+		dvText = wx.StaticText(panel, label='%.2f GB' % (totalData/1024.0**3,))
+		dvText.SetFont(font)
+		
+		sizer.Add(ttText, pos=(row+0, 0), flag=wx.ALIGN_LEFT, border=5)
+		sizer.Add(dvText, pos=(row+0, 2), flag=wx.ALIGN_RIGHT, border=5)
 
-		sum = wx.StaticText(self, -1, '164 102 000', (240, 280))
-		sum_font = sum.GetFont()
-		sum_font.SetWeight(wx.BOLD)
-		sum.SetFont(sum_font)
+		row += 1
 
-		wx.Button(self, 1, 'Ok', (140, 310), (60, 30))
+		ok = wx.Button(panel, ID_VOL_INFO_OK, 'Ok', size=(90, 28))
+		sizer.Add(ok, pos=(row+0, 2))
+		
+		panel.SetSizerAndFit(sizer)
 		
 	def initEvents(self):
-		self.Bind(wx.EVT_BUTTON, self.OnOk, id=1)
+		self.Bind(wx.EVT_BUTTON, self.onOk, id=ID_VOL_INFO_OK)
 		
-	def OnOk(self, event):
+	def onOk(self, event):
 		self.Close()
 
 
