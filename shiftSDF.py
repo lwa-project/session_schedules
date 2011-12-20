9467ceefe6eb6f8b87b18321b79378026fa9e07e
Jayce Dowell <jayce.dowell@gmail.com>
2011-12-20 16:52:58 +0000

    Added LST printouts and a little better error handling to shiftSDF.py.

--- shiftSDF.py
+++ shiftSDF.py
@@ -22,6 +22,7 @@ import pytz
 
 from datetime import datetime
 
+from lsl.common.stations import lwa1
 from lsl.transform import Time
 from lsl.astro import utcjd_to_unix, MJD_OFFSET
 try:
@@ -35,6 +36,7 @@ formatString = '%Y/%m/%d %H:%M:%S.%f %Z'
 
 
 def main(args):
+	observer = lwa1.getObserver()
 	inputSDF  = args[0]
 	outputSDF = args[1]
 	
@@ -49,23 +51,42 @@ def main(args):
 		tStart[i]  = datetime.utcfromtimestamp(tStart[i])
 		tStart[i]  = _UTC.localize(tStart[i])
 	
+	# Get the LST at the start
+	observer.date = (min(tStart)).strftime('%Y/%m/%d %H:%M:%S')
+	lst = observer.sidereal_time()
+	
 	print "File '%s' appears to start at %s" % (inputSDF, (min(tStart)).strftime(formatString))
+	print "-> LST at %s is %s" % (lwa1.name, lst)
 	
 	# Get the few start date/time in UTC and report on the difference
+	print " "
 	print "Enter the new UTC start date/time:"
 	tNewStart = raw_input('YYYY/MM/DD HH:MM:SS.SSS -> ')
 	try:
 		tNewStart = datetime.strptime(tNewStart, '%Y/%m/%d %H:%M:%S.%f')
 	except ValueError:
-		tNewStart = datetime.strptime(tNewStart, '%Y/%m/%d %H:%M:%S')
+		try:
+			tNewStart = datetime.strptime(tNewStart, '%Y/%m/%d %H:%M:%S')
+		except Exception, e:
+			print "Error: %s" % str(e)
+			sys.exit(1)
+			
 	tNewStart = _UTC.localize(tNewStart)
 	tShift = tNewStart - min(tStart)
+	
+	# Get the LST at the new start
+	observer.date = (tNewStart).strftime('%Y/%m/%d %H:%M:%S')
+	lst = observer.sidereal_time()
+	
+	print " "
 	print "Shifting observations to %s (difference of %i days, %.3f seconds)" % (tNewStart.strftime(formatString), tShift.days, (tShift.seconds + tShift.microseconds/1000000.0),)
+	print "-> LST at %s is %s" % (lwa1.name, lst)
 	
 	# Shift the start times and recompute the MJD and MPM values
 	for i in xrange(nObs):
 		tStart[i] += tShift
-		
+	
+	print " "
 	# Update the observations
 	for i in xrange(nObs):
 		utc = Time(tStart[i], format=Time.FORMAT_PY_DATE)
