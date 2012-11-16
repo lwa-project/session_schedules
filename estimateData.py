c75ff922b9bcc2cdfd39cee25cfa59828f0455e1
Jayce Dowell <jayce.dowell@gmail.com>
2012-11-16 21:53:16 +0000

    Added an estimate for the new DR spectrometer data rate to estimateData.py.  The spectrometer mode is specified as 'SPC,tlen,icount' where 'tlen' is the transform length and 'icount' is the number of transforms per integration.

--- estimateData.py
+++ estimateData.py
@@ -5,6 +5,11 @@ Simple script for estimating the data volume for a TBN or DRX observation.
 
 Usage:
    estimateData.py <mode> <filter code>  HH:MM:SS.SSS
+   
+.. note::
+	For spectrometer data, the mode is given by SPC,<tlen>,<icount> where
+	'tlen' is the transform length (channel count) and 'icount' is the 
+	number of transforms per integration.
 
 $Rev$
 $LastChangedBy$
@@ -51,12 +56,34 @@ def main(args):
 		tunepols = 4
 		sampleRate = drxFilters[filterCode]
 		dataRate = 1.0*sampleRate/4096*drxFrameSize*tunepols
+	elif mode[0:3] == 'SPC':
+		try:
+			junk, tlen, icount = mode.split(',', 2)
+		except ValueError:
+			print "Spectrometer settings transform lenght and integration count not specified"
+			sys.exit(1)
+		tlen = int(tlen)
+		icount = int(icount)
+		tunes = 2
+		products = 2
+		sampleRate = drxFilters[filterCode]
+		
+		# Calculate the DR spectrometer frame size
+		headerSize = 64
+		dataSize = tlen*tunes*products*4
+		dataRate = (headerSize+dataSize)/(1.0*tlen*icount/sampleRate)
 	else:
 		print "Unsupported mode: %s" % mode
-		sys.exit(1)		
-
+		sys.exit(1)
+		
 	# Display the final answer
 	print "%s: filter code %i (%i samples/s)" % (mode, filterCode, sampleRate)
+	if mode[0:3] == 'SPC':
+		print "  Channel Count: %i" % tlen
+		print "  Resolution Bandwidth: %.3f Hz" % (1.0*sampleRate/tlen,)
+		print "  Integration Count: %i" % icount
+		print "  Integration Time: %.3f s" % (1.0*tlen*icount/sampleRate,)
+		print "  Polarization Products: %i" % products
 	print "  Data rate: %.2f MB/s" % (dataRate/1024**2,)
 	print "  Data volume for %02i:%02i:%06.3f is %.2f GB" % (hour, minute, second, dataRate*duration/1024**3)
 
