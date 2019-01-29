66e65cf3126e31703c0d22f969b8bdf718cb5b21
Jayce Dowell <jayce.dowell@gmail.com>
2019-01-29 22:35:51 +0000

    Updated the resolveCLI.py script to use Sesame.

--- resolveCLI.py
+++ resolveCLI.py
@@ -11,7 +11,10 @@ Returns an XML file with the coordinates of the target or an error.
 
 import os
 import sys
+import math
+import ephem
 import urllib
+from xml.etree import ElementTree
 
 
 __version__ = "0.1"
@@ -25,28 +28,21 @@ def _resolveSource(name):
     """
     
     try:
-        result = urllib.urlopen('http://www1.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/NameResolver/find?target=%s' % urllib.quote_plus(name))
+        result = urllib.urlopen('https://cdsweb.u-strasbg.fr/cgi-bin/nph-sesame/-oxp/SNV?%s' % urllib.quote_plus(name))
+        tree = ElementTree.fromstring(result.read())
+        target = tree.find('Target')
+        service = target.find('Resolver')
+        coords = service.find('jpos')
         
-        line = result.readlines()
-        target = (line[0].replace('\n', '').split('='))[1]
-        service = (line[1].replace('\n', '').split('='))[1]
-        service = service.replace('\r', '')
-        coordsys = (line[2].replace('\n', '').split('='))[1]
-        coordsys = coordsys.replace('\r', '')
-        ra = (line[3].replace('\n', '').split('='))[1]
-        dec = (line[4].replace('\n', '').split('='))[1]
+        service = service.attrib['name'].split('=', 1)[1]
+        raS, decS = coords.text.split(None, 1)
+        coordsys = 'J2000'
         
-        ra = float(ra)
-        dec = float(dec)
+        ra = ephem.hours(raS) * 180/math.pi
+        dec = ephem.degrees(decS) * 180/math.pi
 
-    except IOError:
-        service = 'Download Error'
-        coordsys = 'NA'
-        ra = -99.99
-        dec = -99.99
-        
-    except ValueError:
-        service = 'Download Error'
+    except (IOError, ValueError, RuntimeError) as e:
+        service = "Error: %s" % str(e)
         coordsys = 'NA'
         ra = -99.99
         dec = -99.99
